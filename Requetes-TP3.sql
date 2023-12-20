6c8735ec88bedeeb992714c989592fee564b5de8
Yasmine Naas <naas.yasmine@courrier.uqam.ca>
2023-12-19 20:16:44 -0500

    modif

--- Requetes-TP3.sql
+++ Requetes-TP3.sql
@@ -1,5 +1,5 @@
 --------------------------Section 2 : Développement des fonctionnalités ----------------------------
---Requête 2.1
+-----------------------------------------Requête 2.1-------------------------------------------------
 CREATE OR REPLACE FUNCTION QuantiteDejaLivree
     (cp_no_produit Commande_Produit.no_produit%TYPE,
     cp_no_commande Commande_Produit.no_commande%TYPE) RETURN NUMBER
@@ -30,5 +30,96 @@ BEGIN
     
     v_quantity_delivered := QuantiteDejaLivree(p_no_produit, p_no_commande);
     DBMS_OUTPUT.PUT_LINE('La quantité livrée: ' || v_quantity_delivered);
+END;
+/
+
+-----------------------------------------Requête 2.2-------------------------------------------------
+
+CREATE SEQUENCE seq_num_livraison
+  START WITH 52000
+  INCREMENT BY 1;
+  
+CREATE OR REPLACE PROCEDURE PreparerLivraison (p_no_client NUMBER)
+IS
+    v_nom_client Client.nom%TYPE;
+    v_prenom_client Client.prenom%TYPE;
+    v_telephone Client.telephone%TYPE;
+    v_id_adresse Client.id_adresse%TYPE;
+    v_adresse VARCHAR(200);
+    v_no_livraison Livraison.no_livraison%TYPE;
+    v_date_livraison Livraison.date_livraison%TYPE;
+
+BEGIN
+    
+    SELECT nom, prenom, telephone, id_adresse
+    INTO v_nom_client, v_prenom_client, v_telephone, v_id_adresse
+    FROM Client
+    WHERE no_client = p_no_client;
+    
+     IF SQL%NOTFOUND THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Le numéro de client spécifié n''existe pas.');
+    END IF;
+    
+
+    SELECT seq_num_livraison.NEXTVAL INTO v_no_livraison FROM dual;
+    INSERT INTO Livraison (no_livraison, date_livraison)VALUES (v_no_livraison, SYSDATE);
+
+    
+    DBMS_OUTPUT.PUT_LINE('Nom du client : ' ||  v_nom_client);
+    DBMS_OUTPUT.PUT_LINE('Prénom du client : ' ||  v_prenom_client);
+    DBMS_OUTPUT.PUT_LINE('Téléphone : ' || v_telephone);
+    SELECT no_civique || ' ' || nom_rue || ', ' || ville || ', ' || pays || ', ' || code_postal
+    INTO v_adresse
+    FROM Adresse
+    WHERE id_adresse = v_id_adresse;
+    DBMS_OUTPUT.PUT_LINE('Adresse : ' || v_adresse);
+    DBMS_OUTPUT.PUT_LINE('Numéro de livraison : ' ||  v_no_livraison);
+    DBMS_OUTPUT.PUT_LINE('Date de livraison : ' || TO_CHAR(SYSDATE, 'DD-MM-YYYY'));
+    
+    FOR prod IN (SELECT CP.no_commande, C.date_commande, P.description, CP.quantite_cmd, CP.no_produit
+                 FROM Commande_Produit CP
+                 INNER JOIN Commande C ON CP.no_commande = C.no_commande
+                 INNER JOIN Produit P ON CP.no_produit = P.no_produit
+                 WHERE C.no_client = p_no_client AND C.statut = 'ENCOURS')
+    LOOP
+        
+        DECLARE
+            v_stock_disponible Produit.quantite_stock%TYPE;
+        BEGIN
+            SELECT quantite_stock INTO v_stock_disponible
+            FROM Produit
+            WHERE no_produit = prod.no_produit;
+
+            
+            IF prod.quantite_cmd <= v_stock_disponible THEN
+                
+                INSERT INTO Livraison_Commande_Produit (no_livraison, no_commande, no_produit, quantite_livree)
+                VALUES (v_no_livraison, prod.no_commande, prod.no_produit, prod.quantite_cmd);
+
+                
+                UPDATE Produit
+                SET quantite_stock = quantite_stock - prod.quantite_cmd
+                WHERE no_produit = prod.no_produit;
+            ELSE
+                
+                NULL; 
+            END IF;
+        END;
+    END LOOP;
+    FOR prod IN (SELECT CP.no_commande, C.date_commande, P.description, CP.quantite_cmd
+                 FROM Commande_Produit CP
+                 INNER JOIN Commande C ON CP.no_commande = C.no_commande
+                 INNER JOIN Produit P ON CP.no_produit = P.no_produit
+                 WHERE C.no_client = p_no_client AND C.statut = 'ENCOURS')
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Commande n°' ||  prod.no_commande ||  ', date commande : ' || prod.date_commande ||
+        ', Description produit : ' || prod.description || ',Quantité à livrer: ' || prod.quantite_cmd);
+    END LOOP;
+    
+    EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Client non trouvé pour le numéro spécifié.');
+    WHEN OTHERS THEN
+        RAISE;
 END;
 /
--- Tests-P-F.md
+++ Tests-P-F.md
@@ -3,3 +3,111 @@
 >Tous les tests se retrouvent dans ('/Tests/Test-Requete-TP3.sql')
 >
 >Prérequis: `Requetes-TP3.sql`
+
+## IMPORTANT AVANT EXCECUTION:
+
+Veillez à exécuter cette commande avant les tests qui suivent:
+
+```
+SET SERVEROUTPUT ON;
+```
+
+Cette commande est utilisee pour activer l'affichage des messages DBMS_OUTPUT.
+
+---
+## Test Requête 2.1:
+
+**Commande SQL :**
+
+```sql
+BEGIN
+    checkdeliveredquantity(302, 201);
+END;
+/ 
+```
+**Résultat Attendu :**
+
+---
+## Test Requête 2.2:
+
+### 1. Test 1: Le no_client existe et a des commandes 'ENCOURS'
+
+**Commande SQL :**
+
+```sql
+DECLARE
+    v_no_client NUMBER := 100; --Remplacez ceci par un numéro de client
+BEGIN
+    PreparerLivraison(v_no_client);
+END;
+/
+``` 
+
+**Résultat Attendu :**
+
+```text
+Nom du client : Tremblay
+Prénom du client : Michel
+Téléphone : (123) 456-7890
+Adresse : 123 Maple Street, Toronto, Canada, M5V 2W6
+Numéro de livraison : (numero livraison)
+Date de livraison : (SYSDATE)
+Commande n°201, date commande : 15-OCT-22, Description produit : Ordinateur portable,Quantité à livrer: 5
+Commande n°201, date commande : 15-OCT-22, Description produit : Smartphone,Quantité à livrer: 2
+Commande n°201, date commande : 15-OCT-22, Description produit : Tablette,Quantité à livrer: 4
+Commande n°202, date commande : 16-OCT-22, Description produit : Clavier,Quantité à livrer: 3
+Commande n°202, date commande : 16-OCT-22, Description produit : Routeur,Quantité à livrer: 1
+Commande n°202, date commande : 16-OCT-22, Description produit : Écran,Quantité à livrer: 4
+```
+
+### 2. Test 2: Le no_client existe mais n'a pas de commandes 'ENCOURS'
+
+**Commande SQL :**
+
+```sql
+DECLARE
+    v_no_client NUMBER := 103; 
+BEGIN
+    PreparerLivraison(v_no_client);
+END;
+/
+```
+
+**Résultat Attendu :**
+
+```text
+Nom du client : Davis
+Prénom du client : Sarah
+Téléphone : (456) 789-0123
+Adresse : 101 Elm Street, Calgary, Canada, T2P 0R5
+Numéro de livraison : (numero livraison)
+Date de livraison : (SYSDATE)
+```
+
+### 3. Test 3: Le no_client n'existe pas 
+
+**Commande SQL :**
+
+```sql
+DECLARE
+    v_no_client NUMBER := 10011; 
+BEGIN
+    PreparerLivraison(v_no_client);
+END;
+/
+```
+
+**Résultat Attendu :**
+
+```text
+Error starting at line : (num) in command -
+DECLARE
+    v_no_client NUMBER := 10011;
+BEGIN
+    PreparerLivraison(v_no_client);
+END;
+Error report -
+ORA-20001: Client non trouvé pour le numéro spécifié.
+ORA-06512: at "CH091976.PREPARERLIVRAISON", line (num)
+ORA-06512: at line (num)
+```
